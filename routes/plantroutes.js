77c1769dace0e80de6c14d5c306832ca18e6dec5
khushi-khushwa <khushikhushwah70@gmail.com>
2026-04-06 21:53:32 +0530

    s

--- routes/plantroutes.js
+++ routes/plantroutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 
-import { getPlants , addPlant, deletePlant, updatePlant} from '../controllers/plantControllers.js';
+import { getPlants , deletePlant, updatePlant} from '../controllers/plantControllers.js';
 
-import upload from "../middlewares/uploadMiddleware.js";
+
 
 
 const router = express.Router();
 
-router.post("/add-product", upload.single("image"), addPlant);
 router.get('/get', getPlants);
 router.delete('/delete/:id', deletePlant);
 router.put('/update/:id', updatePlant)
