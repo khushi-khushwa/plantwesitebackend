cec1753166c33c7cd2d7286c07b83aec12597613
khushi-khushwa <khushikhushwah70@gmail.com>
2026-04-06 22:00:18 +0530

    s

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors({
-  origin: 'https://shyamecommerce.netlify.app/',
+  origin: "*",
   credentials: true,
 }));
 
